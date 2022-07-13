5c5d177c93160f1356beddb156e9a729bb6f0009
Marcos Roberto <marco.2229roberto@gmail.com>
2022-07-13 18:53:18 -0400

    corre~cao login aula

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-// uso do jQuery
-
-$(document).ready(function () {
-
-    // todas as suas funções jQuery
-
-    $(".btn-buy").click(function(){
-        alert("Este produto não está disponível");
-    });
-
-    
-});
-
-
-function abreFechaMenu() {
-
-    // navbar -tag nav
-    
-    let navbar = document.querySelector(".menu");
-    navbar.classList.toggle("mostraEscondeMenu");
-    
-    // console.log(navbar);
-}
-
-
-=======
 function abreFechaMenu() {
 
     // navbar -tag nav
@@ -35,4 +8,3 @@ function abreFechaMenu() {
     
     // console.log(navbar);
 }
->>>>>>> bb3221979f3c0eb021c5794ec668d7619dc13a15
