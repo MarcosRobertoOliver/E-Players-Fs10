8ea94cf439ba047378fd9307c021e2c6001b9fe3
Marcos Roberto de Oliveira <marcosroberto@tce.ms>
2022-07-15 12:24:31 -0400

    modificado jquery aprimorando login criado js

---
+++ js/login.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".signin").click(function(){
+        
+        // //pegar os dados dos campos
+        
+        let cpf = $("#cpf").val();
+        let usuario = $("#usuario").val();
+        let senha = $("#senha").val();
+
+        if(nome.trim() == "" || usuario.trim() == "" || senha == ""){
+            alert("Favor preencher todos os campos");
+        } else {
+            $(".mensagem").text("Dados prontos para cadastrar!!");
+        }
+    });
+});
