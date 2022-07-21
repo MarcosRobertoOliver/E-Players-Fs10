221e6c76a84a8c8b57cd72578039ee8f94f4dd21
Marcos Roberto de Oliveira <marcosroberto@tce.ms>
2022-07-21 10:13:39 -0400

    corrigndo alert em login

--- js/login.js
+++ js/login.js
@@ -1,12 +1,12 @@
+// inicializa o jQuery
 $(document).ready(function(){
-    $("#cpf").Mask("000.000.000-00")
-    
+       
+
     $(".signin").click(function(){
         
         // //pegando os dados dos campos
         
-        
-        let cpf = $("#cpf").val();
+        let nome = $("#cpf").val();
         let usuario = $("#usuario").val();
         let senha = $("#senha").val();
 
@@ -14,8 +14,6 @@ $(document).ready(function(){
             alert("Favor preencher todos os campos");
         } else {
             $(".mensagem").text("Dados prontos para cadastrar!!");
-
-            
         }
     });
 });
